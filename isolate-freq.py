305c27e1050d6e98a22f6ba7d27ebecfbf58720d
Thomas Summers <thomassummers@tjsunr.rd.unr.edu>
2022-06-02 09:32:49 -0700

    have isolate-freq compute autocorrelation

--- isolate-freq.py
+++ isolate-freq.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 import sys, argparse
+from statsmodels.api import tsa
 from collections import defaultdict
 
 parser = argparse.ArgumentParser(description="Used to parse through .csv file containing -NO2 freq information")
 parser.add_argument("-f", help="Inputfile .csv file of MD sim -NO2 Freq info")
 parser.add_argument("-o", default="freq-comp.csv", help="Output .csv name")
+parser.add_argument("-a", default=30, type=int, help="maximum number of lags to compute for the autocorrelation function")
 args = parser.parse_args()
 
 if __name__ == '__main__':
 
     freqinfo = defaultdict(list)
 
+    #Grab relevant freq
     with open(args.f, 'r') as readfile:
         next(readfile)
         for line in readfile:
@@ -20,6 +23,15 @@ if __name__ == '__main__':
                 freqinfo[data[-1].strip()] = [data[0],data[1],data[2],max([abs(float(x)) for x in data[3:12]])]
             elif max([abs(float(x)) for x in data[3:12]]) > freqinfo[data[-1].strip()][3]:
                 freqinfo[data[-1].strip()] = [data[0],data[1],data[2],max([abs(float(x)) for x in data[3:12]])]
+
+    #Calculate autocorrelation from step-ordered names
+    names = [int(x.split("-")[1].split(".")[0]) for x in freqinfo]
+    freqs = [float(freqinfo[x][0]) for x in freqinfo]
+    sort = [x for _,x in sorted(zip(names,freqs))]
+    sortacf = tsa.acf(sort, nlags=args.a)
+    print("Autocorrelation")
+    for item in sortacf:
+        print(item)
 
     with open(args.o, 'w') as savefile:
         savefile.write("Frame,Frequency,Intensity,Force,Max-coord\n")
