6c8d1cb22bf4a3f2308221d64ab4c4a295cfdbe3
tsmmers1 <tjsummers901@gmail.com>
2023-11-10 16:41:33 -0800

    update xyz-to-gro.py

--- xyz-to-gro.py
+++ xyz-to-gro.py
@@ -6,7 +6,7 @@ parser = argparse.ArgumentParser(description="Translates .xyz trajectory to .gro
 parser.add_argument("f", help="Inputfile .xyz file containing coords")
 parser.add_argument("v", help="Inputfile .xyz file containing velocities")
 parser.add_argument("-resname", default="MOL", help="Up to 5-character name for residue, default MOL")
-parser.add_argument("-o", default="merged.gro", help="Name used for output .gro, default merged.gro")
+parser.add_argument("-o", default="converted", help="Name used for output .gro, -pos.xyz, and -vel.xyz, default name: converted")
 args = parser.parse_args()
 
 def extract_coords(cline, vline):
@@ -36,31 +36,46 @@ if __name__ == '__main__':
     Frame = 0
     AtomN = 1
 
-    savefile = open(args.o, 'w')
+    savefileGro = open(args.o+".gro", 'w')
+    savefilePos = open(args.o+"-pos.xyz", 'w')
+    savefileVel = open(args.o+"-vel.xyz", 'w')
 
     for i in range(0,len(coords)):
-        if i == Header: continue
+        if i == Header:
+            savefilePos.write(coords[i])
+            savefileVel.write(veloc[i])
+
         elif i == Header+1:
             if "time = " in coords[i]:
                 time = float(coords[i].split("time =")[1].split(",")[0])
-                savefile.write(f"File constructed from xyz-to-gro.py, t= {time}\n{NAtoms}\n")
+                savefileGro.write(f"File constructed from xyz-to-gro.py, t= {time}\n{NAtoms}\n")
             else:
-                savefile.write(f"File constructed from xyz-to-gro.py, frame= {Frame}\n{NAtoms}\n")
+                savefileGro.write(f"File constructed from xyz-to-gro.py, frame= {Frame}\n{NAtoms}\n")
             Header += 2+NAtoms
             Frame += 1
+            savefilePos.write(coords[i])
+            savefileVel.write(veloc[i])
+
         elif i == Footer:
             #write final coords line
             atom, px, py, pz, vx, vy, vz = extract_coords(coords[i], veloc[i])
-            savefile.write(f'    1{args.resname : <5}{atom : >5}{AtomN : >5}{px : >8.3f}{py : >8.3f}{pz : >8.3f}{vx : >8.4f}{vy : >8.4f}{vz : >8.4f}\n')
-            savefile.write("   2.00000   2.00000   2.00000")
+            savefileGro.write(f'    1{args.resname : <5}{atom : >5}{AtomN : >5}{px : >8.3f}{py : >8.3f}{pz : >8.3f}{vx : >8.4f}{vy : >8.4f}{vz : >8.4f}\n')
+            savefileGro.write("   2.00000   2.00000   2.00000")
             if i < len(coords):
-                savefile.write("\n")
+                savefileGro.write("\n")
             Footer += 2+NAtoms
             AtomN = 1
+            savefilePos.write(f'{atom : >5}  {px : >20.10}  {py : >20.10}  {pz : >20.10}\n')
+            savefileVel.write(f'{atom : >5}  {vx : >20.10}  {vy : >20.10}  {vz : >20.10}\n')
         else:
             atom, px, py, pz, vx, vy, vz = extract_coords(coords[i], veloc[i])
-            savefile.write(f'    1{args.resname : <5}{atom : >5}{AtomN : >5}{px : >8.3f}{py : >8.3f}{pz : >8.3f}{vx : >8.4f}{vy : >8.4f}{vz : >8.4f}\n')
+            savefileGro.write(f'    1{args.resname : <5}{atom : >5}{AtomN : >5}{px : >8.3f}{py : >8.3f}{pz : >8.3f}{vx : >8.4f}{vy : >8.4f}{vz : >8.4f}\n')
             AtomN +=1
-            
-    savefile.close()
+            savefilePos.write(f'{atom : >5}  {px : >20.10}  {py : >20.10}  {pz : >20.10}\n')
+            savefileVel.write(f'{atom : >5}  {vx : >20.10}  {vy : >20.10}  {vz : >20.10}\n')
+
+
+    savefileGro.close()
+    savefilePos.close()
+    savefileVel.close()
 
